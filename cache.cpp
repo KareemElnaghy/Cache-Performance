84eee588c13df018ba75d6e9e16c2c57657bcebd
Kareem <kareemelnaghy@aucegypt.edu>
2024-07-21 17:29:03 +0300

    Added Test Case Functions

--- cache.cpp
+++ cache.cpp
@@ -8,7 +8,7 @@ using namespace std;
 #define		DBG				1
 #define		DRAM_SIZE		(64*1024*1024)
 #define		CACHE_SIZE		(64*1024)
-#define     LINE_SIZE       (16)
+#define     LINE_SIZE       (64)
 
 unsigned int cacheFA[CACHE_SIZE/LINE_SIZE];
 unsigned int cacheDM[CACHE_SIZE/LINE_SIZE];
@@ -28,8 +28,11 @@ unsigned int rand_()
     return (m_z << 16) + m_w;  /* 32-bit result */
 }
 
+// Test Cases
+
 vector<unsigned int> testcase1()
 {
+// Line size: 128
 vector<unsigned int> addr;
 addr.push_back(0x00000000);
 addr.push_back(0x00000010);
@@ -45,6 +48,73 @@ addr.push_back(0x00111110);
 addr.push_back(0x01111100);
 
 return addr;
+}
+
+vector<unsigned int> testcase2()
+{
+// Fully Associative
+// Line size: 16384
+// Test Case to test for capacity miss in fully associative cache
+    vector<unsigned int> addr;
+    for (int i = 0; i < 8; i++)
+    {
+        if(i == 5 || i == 6)
+            addr.push_back(0 + ((i%4) * 16384));
+        else
+            addr.push_back(0 + (i * 16384));
+
+    }
+
+    return addr;
+}
+
+vector<unsigned int> testcase3()
+{
+    // Fully Associative and Direct Mapped
+    // Line Size: 64
+    // Test Case which results in a 100 percent miss rate with DM Cache and with a high hit rate with the FA cache
+    vector<unsigned int> addr;
+    for(int i = 0; i < 64; i++)
+    {
+        addr.push_back(i);
+        addr.push_back(i+CACHE_SIZE);
+    }
+    return addr;
+}
+
+vector<unsigned int> testcase4()
+{
+// Fully Associative
+// Line Size: 64
+// Test Case which access addresses near the boundaries of line
+    vector<unsigned int> addr;
+    for (int i = 0; i < 6; i++)
+    {
+        addr.push_back((i+1)*(64)-1);
+        addr.push_back(((i+1)*(64)));
+    }
+
+    return addr;
+}
+
+vector<unsigned int> testcase5()
+{
+// Direct Mapped
+// Line Size: 64
+// Test Case which tests several features of direct mapped cache i.e. conflict misses and reaccessing previous addr.
+    vector<unsigned int> addr;
+    for (int i = 0; i < 5; i++)
+    {
+        addr.push_back(i*64);
+    }
+    for(int i = 1; i<16; i+=4)
+    {
+        addr.push_back(i*(0x7));
+    }
+    addr.push_back(0x0);
+    addr.push_back(64*1024);
+    addr.push_back((64*1024)+10);
+    return addr;
 }
 
 unsigned int memGen1()
@@ -91,7 +161,6 @@ cacheResType cacheSimDM(unsigned int addr) {
     unsigned int index = (addr >> shamt) & indexx;
     unsigned int tag = addr >> (shamt + indexBits);
 
-
     if (cacheDM[index] == tag && valid[index])
         return HIT;
     else
@@ -135,34 +204,34 @@ int main()
 	cacheResType r;
 
 	// Experiment
-        unsigned int addr;
-        cout << "Cache Simulator\n";
-        for(int inst=0;inst<NO_OF_Iterations;inst++)
-        {
-            addr = memGen6();
-            r = cacheSimFA(addr);
+//        unsigned int addr;
+//        cout << "Cache Simulator\n";
+//        for(int inst=0;inst<NO_OF_Iterations;inst++)
+//        {
+//            addr = memGen6();
+//            //r = cacheSimFA(addr);
 //            r = cacheSimDM(addr);
-
-
-            if(r == HIT) hit++;
-            cout <<"0x" << setfill('0') << setw(8) << hex << addr <<" ("<< msg[r] <<")\n";
-        }
-        cout << "Hit ratio = " << dec<<(100.0*hit/NO_OF_Iterations)<< endl;
+//
+//
+//            if(r == HIT) hit++;
+//            cout <<"0x" << setfill('0') << setw(8) << hex << addr <<" ("<< msg[r] <<")\n";
+//        }
+//        cout << "Hit ratio = " << dec<<(100.0*hit/NO_OF_Iterations)<< endl;
 
     // Test Cases
 
-//    vector<unsigned int> addrT = testcase1();
-//
-//    cout << "Test Case Cache Simulator\n";
-//    for(int inst=0;inst<addrT.size();inst++)
-//    {
-//        r = cacheSimFA(addrT[inst]);
-//        //r = cacheSimDM(addrT[inst]);
-//
-//        if(r == HIT) hit++;
-//        cout <<"0x" << setfill('0') << setw(8) << hex << addrT[inst] <<" ("<< msg[r] <<")\n";
-//    }
-//    cout << "Hit ratio = " << dec<<(100.0*hit/addrT.size())<< endl;
+    vector<unsigned int> addrT = testcase5();
+
+    cout << "Test Case Cache Simulator\n";
+    for(int inst=0;inst<addrT.size();inst++)
+    {
+      // r = cacheSimFA(addrT[inst]);
+       r = cacheSimDM(addrT[inst]);
+
+        if(r == HIT) hit++;
+        cout <<"0x" << setfill('0') << setw(8) << hex << addrT[inst] <<" ("<< msg[r] <<")\n";
+    }
+    cout << "Hit ratio = " << dec<<(100.0*hit/addrT.size())<< endl;
 
 
 }
